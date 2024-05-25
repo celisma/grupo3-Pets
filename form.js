a2270c2e7ad67e6768add96a3c7834dcf3330c35
LucasIbarra <116035989+LucasIbarra@users.noreply.github.com>
2024-05-24 22:15:18 -0300

    arreglos

--- form.js
+++ form.js
@@ -1,12 +1,26 @@
+
 var inputs = document.getElementsByClassName('formulario__input');
 for (var i=0; i < inputs.length; i++) {
     inputs[i].addEventListener('keyup', function() {
         if (this.value.length>=1){
             this.nextElementSibling.classList.add('fijar');
         }
-        else{
-            
+        else{       
             this.nextElementSibling.classList.remove('fijar');  
         }       
     });
 }
+
+
+const nombre = document.getElementById("firtsname")
+const apellido = document.getElementById("surname")
+const email = document.getElementById("email")
+const tel = document.getElementById("phone")
+const formulario = document.getElementById("formulario")
+
+formulario.addEventListener("submit",e=>{
+    e.preventDefault
+    if(firstname.value.length <6){
+        alert("Nombre muy corto")
+    }
+})
